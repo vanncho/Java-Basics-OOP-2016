6bf378d30d9cd03809fc04a998e8581e659e988b
vanncho <van_cho@abv.bg>
2016-07-01 14:44:37 +0300

    corrections

--- 02.Methods/Methods/src/pr10/PR10.java
+++ 02.Methods/Methods/src/pr10/PR10.java
@@ -5,39 +5,50 @@ import java.util.Locale;
 import java.util.Scanner;
 
 class DateModifier {
+    private long diff;
 
-    long differenceBetweenDates(String dayOne, String dayTwo) {
+    public long getDiff() {
+        return diff;
+    }
+
+    void differenceBetweenDates(String dayOne, String dayTwo) {
 
         String[] dayOneTokens = dayOne.split("[\\s]+");
         String[] dayTwoTokens = dayTwo.split("[\\s]+");
 
         Calendar calendar1 = Calendar.getInstance();
-        int c1Year = Integer.parseInt(dayOneTokens[0]);
-        int c1Month = Integer.parseInt(dayOneTokens[1]);
-        int c1Day = Integer.parseInt(dayOneTokens[2]);
-        calendar1.set(c1Year, c1Month, c1Day);
+        calendar1.set(Integer.valueOf(dayOneTokens[0]), Integer.valueOf(dayOneTokens[1]), Integer.valueOf(dayOneTokens[2]));
 
         Calendar calendar2 = Calendar.getInstance();
-        calendar2.set(Integer.parseInt(dayTwoTokens[0]), Integer.parseInt(dayTwoTokens[1]), Integer.parseInt(dayTwoTokens[2]));
+        calendar2.set(Integer.valueOf(dayTwoTokens[0]), Integer.valueOf(dayTwoTokens[1]), Integer.valueOf(dayTwoTokens[2]));
 
-        long startTime = calendar1.getTimeInMillis();
-        long endTime = calendar2.getTimeInMillis();
+        long milliSec1 = calendar1.getTimeInMillis();
+        long milliSec2 = calendar2.getTimeInMillis();
 
-        long timeDifInMilliSec = Math.abs(startTime - endTime);
-        return timeDifInMilliSec / 1000 / 60 /60 / 24;
+        long timeDifInMilliSec;
+        if (milliSec1 >= milliSec2) {
+            timeDifInMilliSec = milliSec1 - milliSec2;
+        } else {
+            timeDifInMilliSec = milliSec2 - milliSec1;
+
+        }
+
+        long timeDifDays = timeDifInMilliSec / (24 * 60 * 60 * 1000);
+        this.diff = timeDifDays;
     }
 }
 
 public class PR10 {
     public static void main(String[] args) {
 
+        Locale.setDefault(new Locale("fr", "FRANCE", "MAC"));
         Scanner input = new Scanner(System.in);
         String first = input.nextLine();
         String second = input.nextLine();
-        Locale.setDefault(new Locale("fr", "FRANCE", "MAC"));
+        //Locale.setDefault(Locale.ROOT);
 
         DateModifier dateModifier = new DateModifier();
-        long res = dateModifier.differenceBetweenDates(first, second);
-        System.out.println(res);
+        dateModifier.differenceBetweenDates(first, second);
+        System.out.println(dateModifier.getDiff());
     }
 }
